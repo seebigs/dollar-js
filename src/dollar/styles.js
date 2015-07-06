1dbca7e436d4762df849ce7d055f162c87096987
isaac woodruff <isaac.woodruff@gmail.com>
2015-07-06 17:59:07 -0400

    IW: added hasClass

--- src/dollar/styles.js
+++ src/dollar/styles.js
@@ -6,21 +6,32 @@ function isObject (obj) {
     return Object.prototype.toString.call(obj) === '[object Object]';
 }
 
+function isFunction (fn) {
+    return Object.prototype.toString.call(fn) === '[object Function]';
+}
 
-// interesting, setting with element.style['property-name'] & element.style['propertyName'] & element.style.propertyName are all the same -- at least in chrome.
-// getting a computed property still requires the exact property name ('background-color' vs 'backgroundColor')
+/*
+ * Styles
+ * - .css()
+ * - .hasClass()
+ * - .addClass(), .removeClass()
+ */
 
+// TODO: make sure setting with numbers works.
 $.fn.css = function (property, value) {
 
     if (!property) {
         return this;
     }
 
+    var i = 0,
+        len;
+
     if (!value) { // getting CSS or setting with object
 
         if (isObject(property)) { // set CSS with object
 
-            for (var i = 0, len = this.length; i < len; i++) {
+            for (len = this.length; i < len; i++) {
                 for (var key in property) {
                     if (property.hasOwnProperty(key)) {
                         this[i].style[key] = property[key];
@@ -34,121 +45,55 @@ $.fn.css = function (property, value) {
             property = typeof property === 'string' ? [property] : property;
             var result = [];
 
-            for (var i = 0, len = property.length; i < len; i++) {
-                property[i] = property[i].replace(/[A-Z]/g, function (match) {
-                    return '-' + match.toLowerCase(); // un-camel case (backgroundColor -> background-color)
-                });
-
-                if (typeof window.getComputedStyle !== 'undefined') {
-                    result.push(window.getComputedStyle(this[0], null).getPropertyValue(property[i]));
-                } else { // IE8 POLYFILL
-                    result.push(getStyleIE8Polyfill(this[0]), property[i]);
-                }
+            for (len = property.length; i < len; i++) {
+                result.push(getStyle(this[0], property[i]));
             }
 
-            return result.length === 1 ? result[0] : result;
+            return result.length === 1 ? result[0] : result; // if multiples, return array -- otherwise just the property
         }
 
     } else { // set string CSS property with string/num value or return from function
 
-        for (var i = 0, len = this.length; i < len; i++) {
-            this[i].style[property[j]] = typeof value === 'string' ? 
-                value : 
-                value.call(this[0], i, getStyle(this[i], property[j]));
+        if (isFunction(value)) {
+            for (len = this.length; i < len; i++) {
+                this[i].style[property] = value.call(this[0], i, getStyle(this[i], property)); // fn gets elem as this and params (index, current style) 
+            }
+        } else {
+            for (len = this.length; i < len; i++) {
+                this[i].style[property] = value;
+            }
         }
 
         return this;
     }
 
-    function getStyleIE8Polyfill (elem, prop) { // IE8 POLYFILL
-        prop = prop === 'float' ? 
-            'styleFloat' : 
-            prop.replace(/^-ms-/, 'ms-').replace(/-([a-z])/gi, function (all, letter) { // insure that property is camel cased
-                return letter.toUpperCase(); // essentially undoing the prior step. but fuck IE8 users.
+    // IE8 POLYFILL:
+    function getStyle (elem, prop) {
+        // while setting CSS can be done with either camel-cased or dash-separated properties
+        // getting computed CSS properties is persnickety about formatting
+
+        if (typeof window.getComputedStyle === 'undefined') { // IE8 POLYFILL
+            prop = prop === 'float' ? 
+                'styleFloat' : 
+                prop.replace(/^-ms-/, 'ms-').replace(/-([a-z])/gi, function (all, letter) { // insure that property is camel cased
+                    return letter.toUpperCase();
+                });
+            return elem.currentStyle[prop];
+        } else {
+            prop = prop.replace(/[A-Z]/g, function (match) { // insure the property is dash-separated
+                return '-' + match.toLowerCase();
             });
-        return elem.currentStyle[prop];
+            return window.getComputedStyle(elem, null).getPropertyValue(prop);
+        }
+    }
+};
+
+$.fn.hasClass = function (className) {
+    for (var i = 0, len = this.length; i < len; i++) {
+        if (this[i].nodeType === 1 && this[i].className.indexOf(className) >= 0) {
+            return true;
+        }
     }
 
-
-    // if (!value) {
-    //     if (typeof properties === 'string' || isArray(properties)) {
-    //         var result = [];
-    //         properties = formatProperties(properties, unCamelCase);
-
-    //         for (var i = 0, len = properties.length; i < len; i++) {
-    //             result.push(getStyle(this[0], properties[i]));
-    //         }
-
-    //         return result.length === 1 ? result[0] : result;
-    //     } else {
-    //         for (var i = 0, len = this.length; i < len; i++) {
-    //             for (var key in properties) {
-    //                 if (properties.hasOwnProperty(key)) {
-    //                     this[i].style[key] = properties[key];
-    //                 }
-    //             }
-    //         }
-    //     }
-    // } else { // set css - accept string, array
-
-    //     // properties = formatProperties(properties, camelCase);
-        
-    //     properties = typeof properties === 'string' ? [properties] : properties;
-
-    //     for (var i = 0, len = this.length; i < len; i++) {
-    //         for (var j = 0, propLen = properties.length; j < propLen; j++) {
-    //             this[i].style[properties[j]] = typeof value === 'string' ? 
-    //                 value : 
-    //                 value.call(this[0], i, getStyle(this[i], properties[j]));
-    //         }
-    //     }
-
-    //     return this;
-    // }
-
-    // // IE8 POLYFILL:
-    // function getStyle (elem, prop) {
-    //     if (typeof window.getComputedStyle === 'undefined') { // IE8 POLYFILL
-    //         prop = prop !== 'float' ? camelCase(prop)[0] : 'styleFloat';
-    //         return elem.currentStyle[prop];
-    //     } else {
-    //         return window.getComputedStyle(elem, null).getPropertyValue(prop);
-    //     }
-    // }
-
-    // function formatProperties (props, format) {
-    //     if (typeof props === 'string') {
-    //         return [format(props)];
-    //     } else {
-    //         // var res = [], 
-    //         //     i = 0, 
-    //         //     len = props.length;
-
-    //         var res = {};
-
-    //         for (var key in props) {
-    //             if (props.hasOwnProperty(key)) {
-    //                 res[key] = format(props[key]);
-    //             }
-    //         }
-
-    //         // for (; i < len; i++) {
-    //         //     res.push(format(props[i]));
-    //         // }
-
-    //         return res;
-    //     }
-    // };
-
-    // function unCamelCase (string) {
-    //     return string.replace(/[A-Z]/g, function (match) {
-    //         return '-' + match.toLowerCase();
-    //     });
-    // }
-
-    // function camelCase (string) {
-    //     return string.replace(/^-ms-/, 'ms-').replace(/-([a-z])/gi, function (all, letter) {
-    //         return letter.toUpperCase();
-    //     });
-    // }
+    return false;
 };
