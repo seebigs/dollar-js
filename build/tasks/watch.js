b9d7f1a11071793663ea41aa70d5dba3d99b8c5d
isaac woodruff <isaac.woodruff@gmail.com>
2015-07-20 18:20:36 -0400

    IW: add jasmine & benchmarks with gulp

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    gutil = require('gulp-util');
 
 gulp.task('watch', function () {
-    gulp.watch('src/**/*.js', ['lint']);
+
+    var tasks = Object.keys(gutil.env)
+    tasks.unshift();
+    tasks.shift('lint');
+
+    gulp.watch('src/**/*.js', tasks);
 });
