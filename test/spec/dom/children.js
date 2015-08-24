5a1fddddc123428011e2eb66c6daefd591d913db
isaac woodruff <isaac.woodruff@gmail.com>
2015-08-24 15:48:16 -0400

    IW: fix test - we support node collection selectors, jQuery does not

--- test/spec/dom/children.js
+++ test/spec/dom/children.js
@@ -1,7 +1,8 @@
 (function () {
     describe('children', function () {
         describe('when passed params', function () {
-            sharedExpectations.compareCollectionForFn('children', ['string', 'dollar']);
+            // another example where we support something different than jQuery
+            sharedExpectations.compareCollectionForFn('children', ['string', /* 'dollar' */]);
         });
     });
 })();
