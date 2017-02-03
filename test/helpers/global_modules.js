3368159f15766e2c8b1713cd684ebe23477cd7b7
Chris Bigelow <seebigs@gmail.com>
2017-02-03 00:21:18 -0500

    CB: upgrade bundl-jasmine-node, require html files directly

--- test/helpers/global_modules.js
+++ test/helpers/global_modules.js
@@ -3,4 +3,4 @@ jQuery = require('../jquery/jquery-3.1.0.js');
 
 $ = require('../../prebuilt/dollar.js');
 
-origHTML = require('./mock_dom_module.js');
+origHTML = require('./mock_dom.html');
--- test/helpers/global_modules_min.js
+++ test/helpers/global_modules_min.js
@@ -3,4 +3,4 @@ jQuery = require('../jquery/jquery-3.1.0.min.js');
 
 $ = require('../../prebuilt/dollar.min.js');
 
-origHTML = require('./mock_dom_module.js');
+origHTML = require('./mock_dom.html');
