1e300095300ca7da971da16389e287fac700d062
isaac woodruff <isaac.woodruff@gmail.com>
2018-02-01 14:04:16 -0500

    IW: npm publish 1.3.11

--- prebuilt/dollar.min.js
+++ prebuilt/dollar.min.js
@@ -1,5 +1,5 @@
 /*!
- * DollarJS 1.3.10 -- a light, fast, modular, jQuery replacement
+ * DollarJS 1.3.11 -- a light, fast, modular, jQuery replacement
  *   Github: https://github.com/seebigs/dollar-js
  *   Released under the MIT license: https://opensource.org/licenses/MIT
  */
