ef29184977dbc38ecec290815967139f7da1232f
Chris Bigelow <seebigs@gmail.com>
2020-03-28 17:25:51 -0400

    CB: publish 2.2.0

--- prebuilt/dollar.js
+++ prebuilt/dollar.js
@@ -1,5 +1,5 @@
 /*!
- * DollarJS 2.1.0 -- a light, fast, modular, jQuery replacement
+ * DollarJS 2.2.0 -- a light, fast, modular, jQuery replacement
  *   Github: https://github.com/seebigs/dollar-js
  *   Released under the MIT license: https://opensource.org/licenses/MIT
  */
